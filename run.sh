9b164a25b576f8f178749a0b4a0abb8d472bc591
Yuki Oshima <oshima.y.0613@gmail.com>
2019-04-09 20:08:01 +0900

    Update run.sh to use Docker Hub

--- run.sh
+++ run.sh
@@ -5,49 +5,24 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+readonly IMAGE_NAME='os1ma/class-diagram-generator-for-java'
+readonly IMAGE_TAG='latest'
+readonly CONTAINER_BASE_DIR="/work"
+readonly CONTAINER_WORKING_DIR="/app"
+readonly PU_FILE="${CONTAINER_BASE_DIR}/output.pu"
+
 readonly SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
-readonly IMAGE_NAME='class-diagram-generator-for-java'
+readonly HOST_BASE_DIR="$1"
+readonly HOST_BASE_DIR_ABSOLUTE="$(cd "${HOST_BASE_DIR}"; pwd)"
 
-exist_target_image() {
-  docker image ls | grep "${IMAGE_NAME}"
-
-  if [[ "$?" != 0 ]]; then
-    echo 'false'
-  else
-    echo 'true'
-  fi
-}
-
-build_image_if_needed() {
-  if [[ "$(exist_target_image)" = 'false' ]]; then
-    docker build -t "${IMAGE_NAME}" "${SCRIPT_DIR}"
-  fi
-}
-
-generate_class_diagram() {
-  local host_base_dir="$1"
-  local container_base_dir="/work"
-  local container_working_dir="/app"
-  local pu_file="${container_base_dir}/output.pu"
-
-  local host_base_dir_absolute="$(cd "${host_base_dir}"; pwd)"
-
-  docker run \
-    --rm \
-    -v "${host_base_dir_absolute}:${container_base_dir}" \
-    -w "${container_working_dir}" \
-    "${IMAGE_NAME}" \
-    bash -c \
-      "java -jar plantuml-dependency-cli-*/plantuml-dependency-cli-*-jar-with-dependencies.jar \
-        --basedir "${container_base_dir}" \
-        -o "${pu_file}" \
-      && sed -i '/ java\./d' "${pu_file}" \
-      && java -jar plantuml.*.jar "${pu_file}""
-}
-
-main() {
-  build_image_if_needed
-  generate_class_diagram "$1"
-}
-
-main "$@"
+docker run \
+  --rm \
+  -v "${HOST_BASE_DIR_ABSOLUTE}:${CONTAINER_BASE_DIR}" \
+  -w "${CONTAINER_WORKING_DIR}" \
+  "${IMAGE_NAME}:${IMAGE_TAG}" \
+  bash -c \
+    "java -jar plantuml-dependency-cli-*/plantuml-dependency-cli-*-jar-with-dependencies.jar \
+      --basedir "${CONTAINER_BASE_DIR}" \
+      -o "${PU_FILE}" \
+    && sed -i '/ java\./d' "${PU_FILE}" \
+    && java -jar plantuml.*.jar "${PU_FILE}""
